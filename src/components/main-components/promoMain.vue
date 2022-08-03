9ee86acf70bb1a5d21617c6f7eed634d5e951b6a
stecala <ste.cala123@gmail.com>
2022-08-03 22:21:36 +0200

    ⭐ Feat (working in progress) : Working on promoMain component in text part

--- src/components/main-components/promoMain.vue
+++ src/components/main-components/promoMain.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="container-lg mt-5">
+  <div class="container-lg mt-5 pt-lg-5 px-5 px-lg-0">
+    <div class="row">
         <div class="col-12 col-lg-6">
-
+            <div class="row px-lg-5">
+                <div class="col-12 pt-lg-5">
+                  <span class="txt-container">
+                    <strong class="text-uppercase">hac tellus, felis risus at </strong>mattis mattis. Eget euismod sempere eget tortor, donec amet, blandit. 
+                    Tristique facilisi faucibus elementum feugiat in nam in feugiat. Ipsum odio etiam duis facilisis amet vulputate.
+                  </span>
+                </div>
+            </div>
         </div>
         <div class="d-none d-lg-block col-lg-6">
             <div class="img-container position-relative">
@@ -11,6 +19,7 @@
                 </div>
             </div>
         </div>
+    </div>
   </div>   
 </template>
 
@@ -21,8 +30,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
 .img-container{
   height: 700px;
+  transition: all 0.5s;
   img{
     width: 100%;
     height: 100%;
@@ -36,5 +47,12 @@ export default {
     font-weight: 700;
     font-size: 0.8rem;
   }
+  &:hover{
+    transform: scale(1.05);
+  }
+}
+.txt-container{
+  line-height: 1.8;
+  font-size: 1.2rem;
 }
 </style>
