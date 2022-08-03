8916df4124f07c923e6c75b0a785c1c1f2b6d801
stecala <ste.cala123@gmail.com>
2022-08-03 13:24:04 +0200

    ⭐ Feat (create component) : create PromoMain

---
+++ src/components/main-components/promoMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container-lg">
+        <div class="col-12 col-lg-6">
+
+        </div>
+        <div class="d-none d-lg-block col-lg-6">
+
+        </div>
+  </div>   
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
